17f2f8954b8e25786ce3a7fb94ad906a436732f5
tina besal <tina.besal@gmail.com>
2022-01-18 10:31:14 +0100

    set grid

--- main.js
+++ main.js
@@ -1,91 +1,27 @@
-HTML CSS JSResult Skip Results Iframe
-EDIT ON
-//minesweeper game by 101computing.net - www.101computing.et/minesweeper-in-javascript/
-var grid = document.getElementById("grid");
-var testMode = false; //Turn this variable to true to see where the mines are
-generateGrid();
+/*  ustvarimo dokument, vsa naša vsebina se bo zapisala znotraj oklepajev, s tem zagotovimo, da se html file loada pred js
+*/
 
-function generateGrid() {
-  //generate 10 by 10 grid
-  grid.innerHTML="";
-  for (var i=0; i<10; i++) {
-    row = grid.insertRow(i);
-    for (var j=0; j<10; j++) {
-      cell = row.insertCell(j);
-      cell.onclick = function() { clickCell(this); };
-      var mine = document.createAttribute("data-mine");       
-      mine.value = "false";             
-      cell.setAttributeNode(mine);
-    }
-  }
-  addMines();
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')  /* piko uporabimo, da povemo js, da iščemo class name. zdaj js ve, da iizbere div ali class iz html dokumenta in ga imenuje grid, da ga lahko uporabimo kasneje v JS kot grid.   */
+ /* const flagsLeft = document.querySelector('#flags-left')
+  const result = document.querySelector('#result') */
+  let width = 10 /* povemo js, da bo mreža iz 10 kvadratkov */
+  let squares = [] /*prazen*/
 
-function addMines() {
-  //Add mines randomly
-  for (var i=0; i<20; i++) {
-    var row = Math.floor(Math.random() * 10);
-    var col = Math.floor(Math.random() * 10);
-    var cell = grid.rows[row].cells[col];
-    cell.setAttribute("data-mine","true");
-    if (testMode) cell.innerHTML="X";
-  }
-}
+    //create Board
+    function createBoard() {     /* poimenujemo funkcijo create board  */
+      for(let i = 0; i < width*width; i++) {/* let i = 0 pomeni, da se bo izvajalo as many time as I want. Ker bi rada, da ima moja mreža 1
+    100 kvadratkov, določim, da se bo loop izvedel 100x oz širina*višina,... i++ pomeni, da se bo izvedel od 0 do 99, po 1 */
+        const square = document.createElement('div')/* <-- to bi radi, da se izvede 100x, da se ustvari 100 divov */
+        square.setAttribute('id', i) /* vsakič, ko se ustvari en kvadratek, mu predpišem svoj id, od 0 do 99, set Attridbute je metoda za to  */
+        grid.appendChild(square) /* dodan square v grid  */
+        squares.push(square)      /* ga poteisnemo v zgornji prazen seznam  */
+      }  
+    } 
 
-function revealMines() {
-    //Highlight all mines in red
-    for (var i=0; i<10; i++) {
-      for(var j=0; j<10; j++) {
-        var cell = grid.rows[i].cells[j];
-        if (cell.getAttribute("data-mine")=="true") cell.className="mine";
-      }
-    }
-}
+    createBoard()
 
-function checkLevelCompletion() {
-  var levelComplete = true;
-    for (var i=0; i<10; i++) {
-      for(var j=0; j<10; j++) {
-        if ((grid.rows[i].cells[j].getAttribute("data-mine")=="false") && (grid.rows[i].cells[j].innerHTML=="")) levelComplete=false;
-      }
-  }
-  if (levelComplete) {
-    alert("You Win!");
-    revealMines();
-  }
-}
-
-function clickCell(cell) {
-  //Check if the end-user clicked on a mine
-  if (cell.getAttribute("data-mine")=="true") {
-    revealMines();
-    alert("Game Over");
-  } else {
-    cell.className="clicked";
-    //Count and display the number of adjacent mines
-    var mineCount=0;
-    var cellRow = cell.parentNode.rowIndex;
-    var cellCol = cell.cellIndex;
-    //alert(cellRow + " " + cellCol);
-    for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-      for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
-        if (grid.rows[i].cells[j].getAttribute("data-mine")=="true") mineCount++;
-      }
-    }
-    cell.innerHTML=mineCount;
-    if (mineCount==0) { 
-      //Reveal all adjacent cells as they do not have a mine
-      for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-        for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
-          //Recursive Call
-          if (grid.rows[i].cells[j].innerHTML=="") clickCell(grid.rows[i].cells[j]);
-        }
-      }
-    }
-    checkLevelCompletion();
-  }
-}
-
-
-
-Resources1× 0.5× 0.25×Rerun
+  })
+    
+    
+    
